8502d498fad06aecde4ecda714865792c7cf6a42
Commondev73 <48597324+Commondev73@users.noreply.github.com>
2022-11-08 21:02:00 +0700

    3 files modified, 1 added, 1 deleted

--- pages/tokens.js
+++ pages/tokens.js
@@ -1,30 +1,80 @@
 import { useState, useEffect } from "react";
+import { isEmpty, sample, sortBy } from "lodash";
+import { EtherScanIcon, LooksRareIcon, OpenseaIcon, X2Y2Icon } from "../svgs";
 import Layout from "../components/layout";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Tokens.module.scss";
 
 const Tokens = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     // query data from your API here
-    fetchData();
+    if (isEmpty(data)) {
+      fetchData();
+    }
   }, []);
 
   const fetchData = async () => {
     try {
       const response = await fetch("/api/get-token");
       const data = await response.json();
-      setData(data);
-    } catch (error) {}
+      if (!isEmpty(data)) {
+        const sortDataByRank = sortBy(data.data, ["rank"]);
+        setData(sortDataByRank);
+      }
+    } catch (_) {}
+  };
+  const imageBuyNow =
+    "https://media.discordapp.net/attachments/903497492878999602/1006808141389910046/buynow-button.png";
+
+  const getImage = (url) => {
+    // const baseUrl = ["https://cloudflare-ipfs.com"];
+    // return `${sample(baseUrl)}/${url.replace("ipfs://", "ipfs/")}`;
+    return null;
   };
 
   return (
     <Layout>
-      <div className={styles.container}>
+      <div className={styles.tokensContainer}>
         <h1>Tokens Table!</h1>
         <hr />
-        {JSON.stringify(data, null, 4)}
         {/* your table implementation goes here */}
+        <div className={styles.tokensTable}>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Rank(N)</th>
+              <th>Buy</th>
+              <th>Link</th>
+              <th>Traits</th>
+            </tr>
+            {!isEmpty(data) &&
+              data.map((data, i) => (
+                <tr key={i}>
+                  <th className={styles.tokensId}>
+                    {data.image && (
+                      <div>
+                        <img src={getImage(data.image)} />
+                      </div>
+                    )}
+                    <span>{data.id}</span>
+                  </th>
+                  <th className={styles.tokensRank}>
+                    <span>{data.rank}</span>
+                  </th>
+                  <th className={styles.tokensBuyNow}>
+                    <img src={imageBuyNow} />
+                  </th>
+                  <th className={styles.tokensLinks}>
+                    <div>
+                      <EtherScanIcon />
+                    </div>
+                  </th>
+                  <th>Traits</th>
+                </tr>
+              ))}
+          </table>
+        </div>
       </div>
     </Layout>
   );
